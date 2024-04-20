fbbfddd3b98a8c1bf30f1900f84d0588c6a40a7d
JoBarn24 <157662244+JoBarn24@users.noreply.github.com>
2024-04-19 17:07:59 -0700

    Set onClickListeners for MainActivity to login and signup pages

--- app/src/main/java/com/example/project2_v3/MainActivity.java
+++ app/src/main/java/com/example/project2_v3/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.project2_v3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.project2_v3.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding =  ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.buttonMainLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                            }
+        });
+
+        binding.buttonMainSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SignupActivity.class));
+            }
+        });
     }
 }
