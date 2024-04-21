0779151509c5c00c63dad70b75ee7097eede02d7
Emily <EmilyGC.0918@gmail.com>
2024-04-20 22:39:42 -0700

    IT'S WORKINGGGGG!!!!!

--- app/src/main/java/com/example/project2_v3/LoginActivity.java
+++ app/src/main/java/com/example/project2_v3/LoginActivity.java
@@ -18,92 +18,50 @@ import com.example.project2_v3.databinding.ActivityLoginBinding;
 public class LoginActivity extends AppCompatActivity {
 
     private ActivityLoginBinding binding;
-
     private MileM8Repository repository;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
-        View view = binding.getRoot();
-        setContentView(view);
+        setContentView(binding.getRoot());
 
         repository = MileM8Repository.getRepository(getApplication());
 
-        binding.signupRedirectTextTextView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                verifyUser();
-            }
-        });
+        binding.signupRedirectTextTextView.setOnClickListener(v -> verifyUser());
 
-        binding.loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-            }
-        });
+        binding.loginButton.setOnClickListener(v -> verifyUser());
     }
-    private void verifyUser(){
+
+    private void verifyUser() {
         String username = binding.loginUsername.getText().toString();
-        if(username.isEmpty()){
-            toastMaker("username should not be blank");
+        if (username.isEmpty()) {
+            toastMaker("Username should not be blank");
             return;
         }
         LiveData<User> userObserver = repository.getUserbyUserName(username);
         userObserver.observe(this, user -> {
-            if(user != null){
+            if (user != null) {
                 String password = binding.loginPassword.getText().toString();
-                if(password.equals(user.getPassword())){
-                   startActivity(MainActivity.mainActivityIntentFactory(getApplicationContext(),user.getId()));
+                if (password.equals(user.getPassword())) {
+                    startActivity(new Intent(LoginActivity.this, LandingActivity.class).putExtra("USER_ID", user.getId()));
+                    finish();  // Optional: Close LoginActivity after redirecting
                 } else {
                     toastMaker("Invalid Password");
                     binding.loginPassword.setSelection(0);
                 }
             } else {
-                toastMaker(String.format("%s is not a valid username. ", username));
+                toastMaker(String.format("%s is not a valid username.", username));
                 binding.loginUsername.setSelection(0);
             }
         });
     }
 
     private void toastMaker(String message) {
-        Toast.makeText(this, message,Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 
-
-    static Intent loginIntentFactory(Context context){
+    static Intent loginIntentFactory(Context context) {
         return new Intent(context, LoginActivity.class);
     }
-
-    private void showLogoutDialog(){
-        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(LoginActivity.this);
-        final AlertDialog alertDialog = alertBuilder.create();
-
-        alertBuilder.setMessage("Do You Want To Logout?");
-
-        alertBuilder.setPositiveButton("Logout", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                logout();
-            }
-        });
-        alertBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                alertDialog.dismiss();
-            }
-        });
-
-        alertBuilder.create().show();
-    }
-
-    private void logout() {
-        //TODO: Finish logout method, wait do i??? it's in main
-        startActivity(LoginActivity.loginIntentFactory(getApplicationContext()));
-    }
 }
-
-
-
-
-
