8b80c8bac70fecc39acc045a430582d8d368f439
Emily <EmilyGC.0918@gmail.com>
2024-04-30 15:46:57 -0700

    connected LandingActivity to EventActivity, calander is bugged out ;-;

--- app/src/main/java/com/example/project2_v3/LandingActivity.java
+++ app/src/main/java/com/example/project2_v3/LandingActivity.java
@@ -2,17 +2,29 @@ package com.example.project2_v3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LandingActivity extends AppCompatActivity {
-
+    private Button eventsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing_page);
 
-    }
+        eventsButton = findViewById(R.id.button_LP_events);
 
+        // Set OnClickListener for the Events Button
+        eventsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LandingActivity.this, EventActivity.class);
+                startActivity(intent);
+    }
+        });
+    }
 
 }
